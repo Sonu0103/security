bd246899343b8a176bf544c91b5985d9909119a0
Sonu0103 <sonuhp2002@gmail.com>
2025-01-19 16:21:29 +0545

    Add Shop component for displaying cricket equipment

---
+++ frontend/src/pages/Shop.jsx
@@ -0,0 +1,12 @@
+function Shop() {
+  return (
+    <div className="container mx-auto px-6 py-8">
+      <h1 className="text-3xl font-bold text-neutral-darkGray mb-8">
+        Cricket Equipment Shop
+      </h1>
+      {/* Shop content will be added here */}
+    </div>
+  );
+}
+
+export default Shop;
